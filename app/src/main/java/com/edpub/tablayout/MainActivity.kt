62e6b8dac62c558769bea4c33c16a024a10db700
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2021-09-01 00:01:14 +0530

    Tab layout working perfectly

--- app/src/main/java/com/edpub/tablayout/MainActivity.kt
+++ app/src/main/java/com/edpub/tablayout/MainActivity.kt
@@ -3,6 +3,8 @@
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
  class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +44,26 @@ import androidx.viewpager2.widget.ViewPager2
 //        viewPager.endFakeDrag()
         //the sequence of the lines must be same for drag
 
+        //to use tab layout its better to use linear layout
+        val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        //we need to use mediator to set icons and badges and also to merge viewpager with tablayout
+        TabLayoutMediator(tabLayout, viewPager){tab, position->
+            tab.text = "Tab ${position}+1"
+        }.attach()
+        //now everything is understandable
+        tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                //called when tab is selected
+            }
 
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                //called when tab is un-selected
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                //called when tab is re-selected
+            }
+        })
 
 
     }
